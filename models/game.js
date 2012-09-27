36eb7cd6afcf92c99a6bb6fc534ab6754b395f8f
Kevin Tseng <kevinktseng@gmail.com>
2012-09-26 17:19:23 -0700

    added some score functionality

--- models/game.js
+++ models/game.js
@@ -1,4 +1,5 @@
-var mongoose = require('mongoose'),
+var async = require('async'),
+    mongoose = require('mongoose'),
     Schema = mongoose.Schema,
     ObjectId = Schema.ObjectId;
 
@@ -7,6 +8,8 @@ var Score = new Schema({
   score: { type: Number, required: true },
   time: { type: Date, default: Date.now }
 });
+
+var MAX_SCORE_LENGTH = 10;
 
 var Game = new Schema({
   name: { type: String, required: true },
@@ -21,5 +24,37 @@ var Game = new Schema({
 });
 
 Game.index({ name: 1 });
+
+/**
+* checks score with current highscores
+* callback(err, weekly_ranking, monthly_ranking, alltime_ranking)
+**/
+Game.methods.checkScore = function(score, callback) {
+  var scoreCheck = function(scores, cb) {
+    if(scores.length === 0) {
+      return cb(null, [ score ]);
+    } else if(score.length < MAX_SCORE_LENGTH) {
+      scores.push(score);
+      scores.sort();
+    } else if(scores[scores.length - 1] < score) {
+      scores[scores.length - 1] = score;
+      scores.sort();
+    }
+    cb(null, scores);
+  };
+  async.parallel([
+    async.apply(this.weeklyScore, scoreCheck),
+    async.apply(this.monthlyScore, scoreCheck),
+    async.apply(this.allTimeScore, scoreCheck)
+  ], function(err, results) {
+    if(err) { return callback(err); }
+    this.weeklyScore = results[0];
+    this.monthlyScore = results[1];
+    this.allTimeScore = results[2];
+    this.save(function(err) {
+      callback(err, results[0].indexOf(score), results[1].indexOf(score), results[2].indexOf(score));
+    });
+  });
+};
 
 module.exports = mongoose.model('Game', Game);
