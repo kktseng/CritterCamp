04b964ec469bc233b6d40c04ea53c9097981f0a0
Kevin Tseng <kevinktseng@gmail.com>
2012-11-06 18:07:50 -0800

    changed mongo config

--- config/production.js
+++ config/production.js
@@ -1,15 +1,15 @@
 module.exports = {
   Redis: {
-    host: "pigbaby.cloudapp.net",
+    host: "localhost",
     port: 6379
   },
 
   Mongo: {
     auto_reconnect: true,
-    db: "pig-data",
-    rs_name: "rs-ds035747",
-    host: [ "ds035747-a.mongolab.com"],
-    port: [ 35747 ],
+    db: "pig-main",
+    rs_name: "rs-ds41177",
+    host: [ "ds041177.mongolab.com"],
+    port: [ 41177 ],
     username: process.env.MONGO_USERNAME,
     password: process.env.MONGO_PASSWORD,
   }
