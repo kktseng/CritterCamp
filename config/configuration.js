829f2c1f27d2818f9c5b55e36a0971f400bf5fa7
Kevin Tseng <kevinktseng@gmail.com>
2012-11-06 18:28:46 -0800

    changed mongo auth

--- config/configuration.js
+++ config/configuration.js
@@ -2,8 +2,8 @@ var config = require('config');
 
 exports.file = config;
 
-exports.mongoUri = function(user, pass, host, port, db) {
-  if (config.Mongo.user && config.Mongo.pass) {
+exports.mongoUri = function() {
+  if (config.Mongo.username && config.Mongo.password) {
     return "mongodb://" + config.Mongo.user + ":" + config.Mongo.pass + "@" + config.Mongo.host + ":" + config.Mongo.port + "/" + config.Mongo.db;
   } else {
     return "mongodb://" + config.Mongo.host + ":" + config.Mongo.port + "/" + config.Mongo.db;
