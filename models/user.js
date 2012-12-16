0d7f9edf4c44c015a71ac0eac575fcf5ee76c833
Kevin Tseng <kevinktseng@gmail.com>
2012-12-16 03:24:53 -0800

    added improvements to group finding

--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ var User = new Schema({
   username: { type: String, required: true },
   password: { type: String },
   email: { type: String },
-  profile: { type: String },
+  profile: { type: String, default: 'pig' },
 
   friends: [ { type: ObjectId, ref: 'User' } ],
   friendRequests: [ { type: ObjectId, ref: 'User' } ],
