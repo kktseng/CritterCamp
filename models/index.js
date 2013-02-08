dd7c815ed5f994a3bff57a1125dcf03552af3031
Kevin Tseng <kevinktseng@gmail.com>
2013-02-08 13:14:09 -0800

    fixed index.js

--- models/index.js
+++ models/index.js
@@ -1,9 +1,6 @@
 exports.News = require('./news');
 
-exports.UserStat = require('./userStat');
 exports.DailyStat = require('./dailyStat');
-
-exports.Achievement = require('./achievement');
 
 exports.User = require('./user');
 exports.Game = require('./game');
