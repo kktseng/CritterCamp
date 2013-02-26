db199d3c7331bba9c3dda68e6acc14f8b8b6785b
Bryan Yasukawa <bryan.yasukawa@gmail.com>
2013-02-25 22:58:43 -0800

    finished script reset_ladder

--- reset_ladder.js
+++ reset_ladder.js
@@ -8,63 +8,70 @@ new mongodb.Db('pig_dev', server, {w: 1}).open(function (error, client) {
   var ranks = new mongodb.Collection(client, 'ranks');
   var leaders = new mongodb.Collection(client, 'leaders');
 
-  var usernames = [ 'pablosanchez', 'petewheeler', 'achmedkhan', 'angeladelvecchio', 'kieshaphillips',
-                    'kennykawaguchi', 'markydubois', 'erniesteele', 'ashleywebber', 'sydneywebber' ];
+  var level_count = 1;
+  var leader_count = 10;
+
+  var usernames = [ 'petewheeler', 'achmedkhan', 'angeladelvecchio', 'kieshaphillips', 'erniesteele',
+                    'kennykawaguchi', 'markydubois', 'ashleywebber', 'sydneywebber', 'pablosanchez' ];
 
   var insert_username = function(username, cb) {
-    console.log('insert');
-    users.insert({ username: username, password: username + '123', email: username + '@gmail.com' }, { safe: true }, function(err, result) {
+    users.insert({ username: username, password: username + '123', email: username + '@gmail.com', level: level_count }, { safe: true }, function(err, result) {
       if(err) { console.warn(err.message); }
-      console.log('cb!?' + cb);
-      return cb();
+      level_count++;
+      cb();
     });
-  }
+  };
+
+  var insert_leader = function(username, cb) {
+    users.find({ username: username }).nextObject(function(err, results) {
+      if(err) { console.warn(err.message); }
+      leaders.insert({ user: results._id, rank: leader_count }, { safe: true }, function(err, result) {
+        if(err) { console.warn(err.message); }
+        leader_count--;
+        cb();
+      });
+    });
+  };
 
   async.waterfall([
-    function(callback) {
-      console.log('1');
-      users.remove({}, callback);
+    function clear_users(callback) {
+      users.remove({}, { safe: true }, function(err, result) {
+        if(err) { console.warn(err.message); }
+        callback();
+      });
     },
-    function(callback) {
-      console.log('2');
-      async.forEach(usernames, insert_username, callback);
+    function insert_dummy_users(callback) {
+      async.forEachSeries(usernames, insert_username, callback);
     },
-    function(callback) {
-      console.log('3');
-      ranks.remove({}, callback);
+    function clear_ranks(callback) {
+      ranks.remove({}, { safe: true }, function(err, result) {
+        if(err) { console.warn(err.message); }
+        callback();
+      });
     },
-    function(callback) {
-      console.log('4');
-      ranks.insert({ rank: 1, level: 1}, { safe: true }, callback);
+    function insert_dummy_rank(callback) {
+      for(var i = 1; i < 11; i++) {
+        (function(level) {
+          ranks.insert({ level: level, rank: 11-level }, { safe: true }, function(err, results) {
+            if(err) { console.warn(err.message); }
+          });
+        })(i);
+      }
+      callback();
     },
-    function(result, callback) {
-      console.log('5');
-      leaders.remove({}, callback);
+    function clear_leaders(callback) {
+      leaders.remove({}, { safe: true }, function(err, result) {
+        if(err) { console.warn(err.message); }
+        callback();
+      });
+    },
+    function insert_dummy_leaders(callback) {
+      async.forEachSeries(usernames, insert_leader, callback);
     }
   ], function(err) {
     if(err) { console.warn(err.message); }
     console.log('Reset ladder success!');
+    server.close();
   });
-/*
-  users.remove({});
-  users.insert({ username: 'pablosanchez', password: 'pablosanchez123', email: 'pablosanchez@gmail.com' });
-  users.insert({ username: 'petewheeler', password: 'petewheeler123', email: 'petewheeler@gmail.com' });
-  users.insert({ username: 'achmedkhan', password: 'achmedkhan123', email: 'achmedkhan@gmail.com' });
-  users.insert({ username: 'angeladelvecchio', password: 'angeladelvecchio123', email: 'angeladelvecchio@gmail.com' });
-  users.insert({ username: 'kieshaphillips', password: 'kieshaphillips123', email: 'kieshaphillips@gmail.com' });
-  users.insert({ username: 'kennykawaguchi', password: 'kennykawaguchi123', email: 'kennykawaguchi@gmail.com' });
-  users.insert({ username: 'markydubois', password: 'markydubois123', email: 'markydubois@gmail.com' });
-  users.insert({ username: 'erniesteele', password: 'erniesteele123', email: 'erniesteele@gmail.com' });
-  users.insert({ username: 'ashleywebber', password: 'ashleywebber123', email: 'ashleywebber@gmail.com' });
-  users.insert({ username: 'sydneywebber', password: 'sydneywebber123', email: 'sydneywebber@gmail.com' });
-
-  ranks.remove({});
-  ranks.insert({ rank: 1, level: 1}, { safe: true }, function(err, result) {
-    if(err) { console.warn(err.message); }
-    console.log('success!');
-  });
-
-  leaders.remove({});
-  */
 
 });
