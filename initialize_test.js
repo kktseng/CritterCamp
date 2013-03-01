cbcdaef59779df361c9cb1ce3a8618fdc5d43470
Bryan Yasukawa <bryan.yasukawa@gmail.com>
2013-03-01 00:09:08 -0800

    fixed bug in initialize_test.js

--- initialize_test.js
+++ initialize_test.js
@@ -10,13 +10,45 @@ new mongodb.Db('pig_dev', server, {w: 1}).open(function (error, client) {
 
   var password_hash = bcrypt.hashSync('password', 12);
 
-  users.insert({ username: 'test_user1', password: password_hash, email: 'test_user1@gmail.com' }, { safe: true }, function(err, result) {
+  users.insert({
+    username: 'test_user1', 
+    password: password_hash, 
+    email: 'test_user1@gmail.com', 
+    profile: 'pig',
+    friends: [],
+    friendRequests: [],
+    achievements: [],
+    gold: 1000,
+    exp: 0,
+    level: 1,
+    dateCreated: Date.now(),
+    lastLogin: Date.now(),
+    loginCount: 0,
+    moneySpent: 0,
+    totalGames: 0
+  }, { safe: true }, function(err, result) {
     if(err) { console.warn(err.message); }
-    users.insert({ username: 'test_user2', password: password_hash, email: 'test_user2@gmail.com'}, { safe: true }, function(err, result) {
-      if(err) { console.warn(err.message); }
-      console.log('Initialize test users success!');
-      server.close();
-    });
+      users.insert({ 
+        username: 'test_user2', 
+        password: password_hash, 
+        email: 'test_user2@gmail.com',
+        profile: 'pig',
+        friends: [],
+        friendRequests: [],
+        achievements: [],
+        gold: 1000,
+        exp: 0,
+        level: 1,
+        dateCreated: Date.now(),
+        lastLogin: Date.now(),
+        loginCount: 0,
+        moneySpent: 0,
+        totalGames: 0
+      }, { safe: true }, function(err, result) {
+        if(err) { console.warn(err.message); }
+        console.log('Initialize test users success!');
+        server.close();
+      });
   });
 
 });
