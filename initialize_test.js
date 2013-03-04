68fd4da17265112ab4e4f1530ce4ee55670e61a0
Bryan Yasukawa <bryan.yasukawa@gmail.com>
2013-03-03 23:53:47 -0800

    added starry night and jetpack jamboree to initialize test script

--- initialize_test.js
+++ initialize_test.js
@@ -7,6 +7,7 @@ var server = new mongodb.Server("127.0.0.1", 27017, {});
 new mongodb.Db('pig_dev', server, {w: 1}).open(function (error, client) {
   if (error) throw error;
   var users = new mongodb.Collection(client, 'users');
+  var games = new mongodb.Collection(client, 'games');
 
   var password_hash = bcrypt.hashSync('password', 12);
 
@@ -29,28 +30,34 @@ new mongodb.Db('pig_dev', server, {w: 1}).open(function (error, client) {
     totalGames: 0
   }, { safe: true, upsert: true }, function(err, result) {
     if(err) { console.warn(err.message); }
-      users.update({ username: 'test_user2' },
-      { 
-        username: 'test_user2', 
-        password: password_hash, 
-        email: 'test_user2@gmail.com',
-        profile: 'pig',
-        friends: [],
-        friendRequests: [],
-        achievements: [],
-        gold: 1000,
-        exp: 0,
-        level: 1,
-        dateCreated: Date.now(),
-        lastLogin: Date.now(),
-        loginCount: 0,
-        moneySpent: 0,
-        totalGames: 0
-      }, { safe: true, upsert: true }, function(err, result) {
+    users.update({ username: 'test_user2' },
+    { 
+      username: 'test_user2', 
+      password: password_hash, 
+      email: 'test_user2@gmail.com',
+      profile: 'pig',
+      friends: [],
+      friendRequests: [],
+      achievements: [],
+      gold: 1000,
+      exp: 0,
+      level: 1,
+      dateCreated: Date.now(),
+      lastLogin: Date.now(),
+      loginCount: 0,
+      moneySpent: 0,
+      totalGames: 0
+    }, { safe: true, upsert: true }, function(err, result) {
+      if(err) { console.warn(err.message); }
+      games.update({ name: 'jetpack_jamboree' }, { name: 'jetpack_jamboree' }, { safe: true, upsert: true }, function(err, result) {
         if(err) { console.warn(err.message); }
-        console.log('Initialize test users success!');
-        server.close();
+        games.update({ name: 'starry_night' }, { name: 'starry_night' }, { safe: true, upsert: true }, function(err, result) {
+          if(err) { console.warn(err.message); }
+          console.log('Initialize test users success!');
+          server.close();
+        });
       });
+    });
   });
 
 });
--- models/game.js
+++ models/game.js
@@ -13,56 +13,12 @@ var MAX_SCORE_LENGTH = 10;
 
 var Game = new Schema({
   name: { type: String, required: true },
-
-  maxPartySize: { type: Number, default: 1 },
-  version: { type: Number, default: 0.0 },
-
-  // scores
-  weeklyScore: { type: [ Score ], default: [] },
-  monthlyScore: { type: [ Score ], default: [] },
-  allTimeScore: { type: [ Score ], default: [] }
 });
 
 Game.index({ name: 1 });
 
 function scoreSort(a, b) {
   return a.score <= b.score;
-}
-
-/**
-* checks score with current highscores
-*
-* callback(err, weekly_ranking, monthly_ranking, alltime_ranking)
-**/
-Game.methods.checkScore = function(user, score, callback) {
-  var self = this;
-  score = { user: user, score: score};
-  var scoreCheck = function(scores, cb) {
-    scores = scores.slice();
-    if(scores.length === 0) {
-      return cb(null, [ score ]);
-    } else if(scores.length < MAX_SCORE_LENGTH) {
-      scores.push(score);
-      scores.sort(scoreSort);
-    } else if(scores[0].score < score.score) {
-      scores[0] = score;
-      scores.sort(scoreSort);
-    }
-    cb(null, scores);
-  };
-  async.parallel([
-    async.apply(scoreCheck, this.weeklyScore),
-    async.apply(scoreCheck, this.monthlyScore),
-    async.apply(scoreCheck, this.allTimeScore)
-  ], function(err, results) {
-    if(err) { return callback(err); }
-    self.weeklyScore = results[0];
-    self.monthlyScore = results[1];
-    self.allTimeScore = results[2];
-    self.save(function(err) {
-      callback(err, results[0].indexOf(score) + 1, results[1].indexOf(score) + 1, results[2].indexOf(score) + 1);
-    });
-  });
 };
 
 module.exports = mongoose.model('Game', Game);
