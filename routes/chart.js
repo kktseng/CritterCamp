a2f5ece81bf167d15026b5eaaed832f5ada8c39b
Bryan Yasukawa <bryan.yasukawa@gmail.com>
2013-04-06 16:40:38 -0700

    git added routes/chart.js

---
+++ routes/chart.js
@@ -0,0 +1,18 @@
+var async = require('async'),
+    globals = require('../lib/globals'),
+    users = require('../lib/users'),
+    helpers = require('../lib/helpers');
+
+module.exports = function(app, basepath) {
+  app.get(basepath + '/dailystat', function(req, res) {
+    var days = req.param('days');
+    var game = req.param('game');
+
+    res.send("google.visualization.Query.setResponse({version:'0.6',reqId:'0',status:'ok',sig:'5982206968295329967',table:{cols:[{id:'Col1',label:'',type:'number'},{id:'Col2',label:'',type:'number'},{id:'Col3',label:'',type:'number'}],rows:[{c:[{v:1.0,f:'1'},{v:2.0,f:'2'},{v:3.0,f:'3'}]},{c:[{v:2.0,f:'2'},{v:3.0,f:'3'},{v:4.0,f:'4'}]},{c:[{v:3.0,f:'3'},{v:4.0,f:'4'},{v:5.0,f:'5'}]},{c:[{v:1.0,f:'1'},{v:2.0,f:'2'},{v:3.0,f:'3'}]}]}});");
+  });
+
+  app.get(basepath + '/hourlystat', function(req, res) {
+    res.send('Trying to create a new account!');
+  });
+
+};
