12e210bdd8f22ecf212622e99424fd7b91c041c8
Bryan Yasukawa <bryan.yasukawa@gmail.com>
2013-05-21 18:17:10 -0700

    added profile to login response

--- routes/login.js
+++ routes/login.js
@@ -42,6 +42,7 @@ module.exports = function(app, basepath) {
           friends: results[1], 
           requests: results[2],
           rank: results[3].rank,
+          profile: auth_user.profile,
           level: auth_user.level, 
           curr_lvl_exp: globals.EXP_TO_LEVEL[auth_user.level - 1],
           next_lvl_exp: globals.EXP_TO_LEVEL[auth_user.level],
